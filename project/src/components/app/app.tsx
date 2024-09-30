e9f640666e5d10b7b0a8e8360aba85710ca8a00b
Yana Rozova <rozova.ys@gmail.com>
2024-09-30 18:27:10 +0300

    Add login screen

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,14 @@
-import MainScreen from '../../pages/main-screen/main-screen';
+// import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
 
 type AppProps = {
   filmCount: number;
 };
 
 function App({ filmCount }: AppProps): JSX.Element {
-  return <MainScreen filmCount={filmCount} />;
+  // return <MainScreen filmCount={filmCount} />;
+  return <LoginScreen />;
+
 }
 
 export default App;
