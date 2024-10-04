462b1eb4cbb1df3162ca3cbb4470c4e4e822533d
Yana Rozova <rozova.ys@gmail.com>
2024-10-04 17:39:42 +0300

    Add review screen

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,8 @@
 // import MainScreen from '../../pages/main-screen/main-screen';
 // import LoginScreen from '../../pages/login-screen/login-screen';
 // import MyListScreen from '../../pages/my-list-screen/my-list-screen';
-import FilmScreen from '../../pages/film-screen/film-screen';
+// import FilmScreen from '../../pages/film-screen/film-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
 
 type AppProps = {
   filmCount: number;
@@ -12,7 +13,8 @@ type AppProps = {
 function App({ filmCount, favoriteFilmCount, similarFilmCount }: AppProps): JSX.Element {
   // return <MainScreen filmCount={filmCount} />;
   // return <MyList favoriteFilmCount={favoriteFilmCount} />;
-  return <FilmScreen similarFilmCount={similarFilmCount} />;
+  // return <FilmScreen similarFilmCount={similarFilmCount} />;
+  return <AddReviewScreen />;
 }
 
 export default App;
