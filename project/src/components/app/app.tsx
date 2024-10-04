cea8036c57239d3716a5a667deb3a1a88ec87f77
Yana Rozova <rozova.ys@gmail.com>
2024-10-04 17:51:47 +0300

    Add player screen

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,7 +2,8 @@
 // import LoginScreen from '../../pages/login-screen/login-screen';
 // import MyListScreen from '../../pages/my-list-screen/my-list-screen';
 // import FilmScreen from '../../pages/film-screen/film-screen';
-import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+// import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
 
 type AppProps = {
   filmCount: number;
@@ -14,7 +15,7 @@ function App({ filmCount, favoriteFilmCount, similarFilmCount }: AppProps): JSX.
   // return <MainScreen filmCount={filmCount} />;
   // return <MyList favoriteFilmCount={favoriteFilmCount} />;
   // return <FilmScreen similarFilmCount={similarFilmCount} />;
-  return <AddReviewScreen />;
+  return <PlayerScreen />;
 }
 
 export default App;
