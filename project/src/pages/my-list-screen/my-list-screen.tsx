5ce70be4a94b4939c5a21093e6c269242b7a97cf
Yana Rozova <rozova.ys@gmail.com>
2024-10-11 18:26:39 +0300

    Add logo component to my-list-screen

--- project/src/pages/my-list-screen/my-list-screen.tsx
+++ project/src/pages/my-list-screen/my-list-screen.tsx
@@ -1,20 +1,17 @@
 import Card from '../../components/card/card';
+import Logo from '../../components/logo/logo';
 
 type MyListProps = {
   favoriteFilmCount: number;
 };
 
 function MyListScreen({ favoriteFilmCount }: MyListProps): JSX.Element {
+  const isFooterLogo = true;
+
   return (
     <div className='user-page'>
       <header className='page-header user-page__head'>
-        <div className='logo'>
-          <a href='main.html' className='logo__link'>
-            <span className='logo__letter logo__letter--1'>W</span>
-            <span className='logo__letter logo__letter--2'>T</span>
-            <span className='logo__letter logo__letter--3'>W</span>
-          </a>
-        </div>
+        <Logo />
 
         <h1 className='page-title user-page__title'>My list</h1>
 
@@ -43,13 +40,8 @@ function MyListScreen({ favoriteFilmCount }: MyListProps): JSX.Element {
       </section>
 
       <footer className='page-footer'>
-        <div className='logo'>
-          <a href='main.html' className='logo__link logo__link--light'>
-            <span className='logo__letter logo__letter--1'>W</span>
-            <span className='logo__letter logo__letter--2'>T</span>
-            <span className='logo__letter logo__letter--3'>W</span>
-          </a>
-        </div>
+
+        <Logo isLight={isFooterLogo} />
 
         <div className='copyright'>
           <p>© 2019 What to watch Ltd.</p>
