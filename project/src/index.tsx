7355446b787e2b2365600471e025c4fdf80eb67d
Yana Rozova <rozova.ys@gmail.com>
2025-02-12 17:35:05 +0300

    Dispatch auth and films list

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { setFilms, setReviews } from './store/actions';
-import { checkAuthAction } from './store/api-actions';
+// import { setFilms, setReviews } from './store/actions';
+import { checkAuthAction, fetchFilmsAction } from './store/api-actions';
 
 import { store } from './store';
-import { filmsList } from './mocks/films';
-import { reviewsList } from './mocks/review';
 
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
 
 store.dispatch(checkAuthAction());
-store.dispatch(setFilms(filmsList));
-store.dispatch(setReviews(reviewsList));
+store.dispatch(fetchFilmsAction());
+// store.dispatch(setFilms(filmsList));
+// store.dispatch(setReviews(reviewsList));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
