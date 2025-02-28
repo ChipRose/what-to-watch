2a93d90d9a17469e0aebd0d732386d7f633cfbed
Yana Rozova <rozova.ys@gmail.com>
2025-02-28 18:25:17 +0300

    Set validation on review Form

--- project/src/components/add-review-form/add-review-form.tsx
+++ project/src/components/add-review-form/add-review-form.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/use-app-dispatch';
 
-import type { FilmIdType } from '../../types/film';
-import type { ChangeTextareaEvent, ChangeInputEvent, FormEvent } from '../../types/form';
+import { type FilmIdType } from '../../types/film';
+import { type ChangeTextareaEvent, type ChangeInputEvent, type FormEvent, type TextArea, type Input } from '../../types/form';
 
 import { fetchNewReviewAction } from '../../store/api-actions';
+import { AppRoute } from '../../const/const';
 
 export type NewReviewType = {
   rating: number;
@@ -16,13 +19,20 @@ type RatingScaleProps = {
   onUpdate: ({ rating }: { rating: number }) => void;
 }
 
-type AddReviewFormProps={
+type AddReviewFormProps = {
   filmId: FilmIdType | null;
 }
 
 const STARS_COUNT = 10;
 
+export enum Validity {
+  MinCommentLength = 50,
+  MaxCommentLength = 400,
+}
+
 function RatingScale({ rating, onUpdate }: RatingScaleProps): JSX.Element {
+  const ratingRef = useRef<Input>(null);
+
   const starsProps = Array.from({ length: STARS_COUNT }, (_, idx) => ({
     value: STARS_COUNT - idx, name: `Rating${idx}`, id: `star-${STARS_COUNT - idx}`
   }));
@@ -31,6 +41,13 @@ function RatingScale({ rating, onUpdate }: RatingScaleProps): JSX.Element {
 
   const handleInputChange = (evt: ChangeInputEvent) => {
     const { value }: { value: string } = evt.target as HTMLInputElement;
+
+    if (ratingRef && value) {
+      ratingRef?.current?.setCustomValidity('It\'s need to set rating');
+      ratingRef?.current?.reportValidity();
+    } else {
+      ratingRef?.current?.setCustomValidity('');
+    }
 
     onUpdate({ rating: Number(value) });
   };
@@ -42,6 +59,7 @@ function RatingScale({ rating, onUpdate }: RatingScaleProps): JSX.Element {
           <React.Fragment key={id}>
             <input
               className="rating__input"
+              ref={ratingRef}
               id={id}
               type="radio"
               name="rating"
@@ -57,9 +75,17 @@ function RatingScale({ rating, onUpdate }: RatingScaleProps): JSX.Element {
   );
 }
 
+
 function AddReviewForm({ filmId }: AddReviewFormProps): JSX.Element {
   const dispatch = useAppDispatch();
-  const [formData, setFormData] = useState<NewReviewType>({ rating: 0, comment: '' });
+  const navigate = useNavigate();
+
+  const textareaRef = useRef<TextArea>(null);
+
+  const initFormState: NewReviewType = { rating: 0, comment: '' };
+  const [isValid, setIsValid] = useState<boolean>(false);
+  const [formData, setFormData] = useState<NewReviewType>(initFormState);
+  const [buttonState, setButtonState] = useState<{ disabled: boolean }>({ disabled: true });
 
   const onInputChange = ({ rating }: { rating: number }) => {
     setFormData((prevState) => ({ ...prevState, rating }));
@@ -67,14 +93,38 @@ function AddReviewForm({ filmId }: AddReviewFormProps): JSX.Element {
 
   const handleTextAreaChange = (evt: ChangeTextareaEvent) => {
     const { id, value }: { id: string; value: string } = evt.target as HTMLTextAreaElement;
-
+    if (textareaRef && (value.length < Validity.MinCommentLength || value.length > Validity.MaxCommentLength)) {
+      textareaRef?.current?.setCustomValidity(`It's need ${Validity.MinCommentLength} - ${Validity.MaxCommentLength} symbols.`);
+      textareaRef?.current?.reportValidity();
+    } else {
+      textareaRef?.current?.setCustomValidity('');
+    }
     setFormData((prevState) => ({ ...prevState, [id]: value }));
   };
 
   const handleSubmit = (evt: FormEvent): void => {
     evt.preventDefault();
-    filmId && dispatch(fetchNewReviewAction({ id: filmId, ...formData }));
+    if (filmId) {
+      dispatch(fetchNewReviewAction({ id: filmId, ...formData }));
+      setFormData(initFormState);
+      navigate(`${AppRoute.Films}/${filmId}`);
+    }
   };
+
+  useEffect(() => {
+    const isCommentValid = formData.comment?.length >= Validity.MinCommentLength && formData.comment?.length <= Validity.MaxCommentLength;
+    const isRatingValid = Boolean(formData.rating);
+    if (isCommentValid && isRatingValid) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    setButtonState({ disabled: !isValid });
+  }, [isValid]);
+
 
   return (
     <div className="add-review">
@@ -85,6 +135,7 @@ function AddReviewForm({ filmId }: AddReviewFormProps): JSX.Element {
 
         <div className="add-review__text">
           <textarea
+            ref={textareaRef}
             className="add-review__textarea"
             name="review-text"
             id="comment"
@@ -93,7 +144,7 @@ function AddReviewForm({ filmId }: AddReviewFormProps): JSX.Element {
             value={formData.comment}
           />
           <div className="add-review__submit">
-            <button className="add-review__btn" type="submit">Post</button>
+            <button className="add-review__btn" type="submit" {...buttonState}>Post</button>
           </div>
 
         </div>
--- project/src/types/form.ts
+++ project/src/types/form.ts
@@ -2,4 +2,5 @@ export type ChangeInputEvent = React.ChangeEvent<HTMLInputElement>
 export type ChangeTextareaEvent = React.ChangeEvent<HTMLTextAreaElement>
 export type FormEvent = React.FormEvent<HTMLFormElement>
 export type LinkEvent = React.MouseEvent<HTMLAnchorElement>;
-export type Input=HTMLInputElement;
+export type Input = HTMLInputElement;
+export type TextArea = HTMLTextAreaElement;
